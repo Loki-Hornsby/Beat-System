0681c5a2ad5b775a329538e0f9b226f84fa69225
Loki hornsby <lokihornsby@gmail.com>
2022-12-12 23:50:38 +0000

    Add Thread Handling dependency

---
+++ Loki's Beat System/ThreadHandling.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
+
+public static class ThreadHandling {
+    public delegate void Threading();
+    public static event Threading Finished;
+
+    static Queue<Action> tasks = new Queue<Action>();
+
+    public static void QueueTask(Action action){
+        tasks.Enqueue(action);
+    }
+
+    public static void ExecuteTasks(){
+        RunNextTask();
+    }
+
+    // Runs the next task queued in [tasks]
+    async static void RunNextTask(int timeout = 10){
+        // Timeout in ms
+        timeout = timeout * 1000;
+
+        // Task creation
+        var task = Task.Run(tasks.Dequeue());
+        await task.ContinueWith(t => Debug.Log("TASK DONE"));
+
+        // Timeout
+        if (await Task.WhenAny(task, Task.Delay(timeout)) == task) {
+            // Task completed without timing out
+            if (tasks.Count > 0){
+                RunNextTask();
+            } else {
+                Finished?.Invoke(); 
+            }
+        } else { 
+            // Task timed out
+            Debug.LogError("A timeout occurred in Analysis.cs");
+        }
+    }
+}
